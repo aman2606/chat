871ef2faa1b27b5481ae68ab5b7d9124e119f649
Aman Sharma <amans@GRAZITTI.LOCAL>
2025-06-18 14:53:59 +0530

    Added versal json file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const webRoutes = require('./routes/web');
 const app = express();
 
 app.set("view engine","ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
